2b45bdc052468ee80435627e6a649756e91c71a9
scarlett <nickodelacruz96@gmail.com>
2020-03-13 04:21:36 -0700

    first my first function

--- scarpers.js
+++ scarpers.js
@@ -1,6 +1,22 @@
 var five = 5;
 var sum = 6;
 
+const happy = "Hi";
+const sad = 30;
+
 var z = (five+sum);
 console.log(z);
 
+
+console.log(happy + sad);
+
+function testHum(a, b) {
+    if (a === b) {
+        return "its 1 bro";
+
+    } else  {
+        return "idk";
+    }
+}
+
+console.log(testHum(4,5))
